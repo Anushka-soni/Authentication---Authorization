c0f6e2c49a2b8fc70699904387613e65d092b61d
Anushka <3anushka(@gmail.com>
2026-01-09 23:56:55 +0530

    feat: initialize FastAPI app with database setup and question models.

---
+++ models.py
@@ -0,0 +1,8 @@
+from sqlalchemy import Boolen, Column, ForeignKey, Integer, String
+from database import Base
+
+class Questions(Base):
+    __tablename__ = "questions"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    question_text = Column(String, index=True) 
